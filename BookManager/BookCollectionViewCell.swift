15b1c266a839da1e1faab1662fcf8b38de8452c2
Glsme <glasses.str.man@gmail.com>
2022-07-20 15:24:03 +0900

    Initial Commit

---
+++ BookManager/BookCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  BookCollectionViewCell.swift
+//  BookManager
+//
+//  Created by Seokjune Hong on 2022/07/20.
+//
+
+import UIKit
+
+class BookCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ BookManager/BookCollectionViewController.swift
@@ -0,0 +1,40 @@
+//
+//  BookCollectionViewController.swift
+//  BookManager
+//
+//  Created by Seokjune Hong on 2022/07/20.
+//
+
+import UIKit
+
+class BookCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let layout = UICollectionViewFlowLayout()
+        let spacing: CGFloat = 8
+        let bookWidth = UIScreen.main.bounds.width - (spacing * 4)
+        layout.itemSize = CGSize(width: bookWidth / 2, height: bookWidth / 2)
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        collectionView.collectionViewLayout = layout
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BookCollectionViewCell", for: indexPath) as! BookCollectionViewCell
+        
+        cell.layer.cornerRadius = 16
+        cell.backgroundColor = .purple
+        
+        return cell
+    }
+    
+}
